51c9a66dd91deb6072f5c33371f6800651a2001e
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-09 16:07:52 -0700

    Add selection dropdowns

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,8 +1,8 @@
 /*
 use an array of arrays to store the gameboard status
-var board = [['o','o','o'], 
-             ['o','o','o'],
-             ['o','o','o']]
+var board = [['.','.','.'], 
+             ['.','.','.'],
+             ['.','.','.']]
 */
 
 /*
@@ -15,8 +15,16 @@ when user1 hits submit, it will pass the dropdown's values and the currentUser i
 
 
 */
+var currentUser = 'player1';
 
-
+var addPiece = function(piece, horiz, vert) {
+  if (gameboard[horiz][vert] === '.' ) {
+    gameboard[horiz][vert] = piece;
+    /*todo: toggle to the other player*/
+  } else {
+    /*todo: change the HTML of the message*/
+  }
+};
 /*function addPiece
 i: a game piece (x or o), horizontal coordinate (0-2), and vertical coordinate (0-2)
 o: none, but modifies the gameBooard array as a side effect
@@ -30,3 +38,12 @@ check the coordinate (aka the array at [horiz, vert])
   else (the space is occuped)
     display a message telling the user that the space is occupied and to choose new coordinates
 */
+
+/*function messageChanger()
+if gameOver && win
+  change msgText to Player [] Won!
+else if gameOver && draw
+  change msgText to draw
+else if function addpiece 
+  change msgText to invalid move*/
+
