855b3d75fd04d3b2327b77f1d4b3e5873d6d96a1
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-09 11:58:14 -0700

    Notes about app/game flow

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,32 @@
+/*
+use an array of arrays to store the gameboard status
+var board = [['o','o','o'], 
+             ['o','o','o'],
+             ['o','o','o']]
+*/
+
+/*
+var currentUser = 'playerX';
+if playerX
+var showCoordinateSelector(player) player arg = currentUser
+display the section where currentUser can choose their part
+this section will have coordinate selection dropdowns
+when user1 hits submit, it will pass the dropdown's values and the currentUser into the addPiece function
 
 
+*/
+
+
+/*function addPiece
+i: a game piece (x or o), horizontal coordinate (0-2), and vertical coordinate (0-2)
+o: none, but modifies the gameBooard array as a side effect
+c: none
+e: none
+
+check the coordinate (aka the array at [horiz, vert])
+  if the value is . (aka nothing is there)
+    change the value to piece's value
+    switch to the next user's turn (currentUser = the other player)
+  else (the space is occuped)
+    display a message telling the user that the space is occupied and to choose new coordinates
+*/
