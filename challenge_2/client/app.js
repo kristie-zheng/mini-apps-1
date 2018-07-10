d6ef0f473a91fc80adb6cb9a73273142c8411193
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-10 12:20:42 -0700

    Make function that executes post request on submit

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,19 +1,57 @@
-//set up express
-var express = require('express');
-var app = express();
+//var $ = import('jquery');
+// //set up express
+// var express = require('express');
+// var app = express();
 
-//serve the static HTML file
-app.use(express.static('client'));
+// //serve the static HTML file
+// app.use(express.static('client'));
 
-//get request
-app.get('/', function(request, response) {
-  response.send('csv app');
-});
+// //get request
+// app.get('/', function(request, response) {
+//   response.send('csv app');
+// });
 
 
-//post request
+// //post request
 
 //start up the server
-app.listen(3000, function() {
-  console.log('app listening on port 3k');
+// app.listen(3000, function() {
+//   console.log('app listening on port 3k');
+// });
+
+var dataJSON;
+
+var submitHandler = function() {
+  var dataToSubmit= $('dataSubmission').val();
+  var dataJSON = JSON.stringify(dataToSubmit); //this is what will be sent with the post request
+  //POST request that submits stringified data to server 
+  $.ajax('/', {
+    method: 'POST',
+    contentType: 'application/JSON',
+    data: dataJSON,
+    //dataType: //what you expect to receive back
+    success: function(data) {
+      console.log('this data was posted', data);
+    },
+    error: function(error) {
+      console.log('failed to post', error);
+    }
+  });
+};
+
+
+// //get request that receives the JSON from server to be converted into CSV
+$.ajax('/', {
+  method: 'GET',
+  contentType: 'application/JSON',
+  data: dataJSON,
+  //dataType: //what you expect to receive back
+  success: function(data) {
+    console.log(JSON.parse(data));
+  },
+  error: function(error) {
+    console.log('failed to get', error);
+  }
 });
+
+console.log($(document.body));
