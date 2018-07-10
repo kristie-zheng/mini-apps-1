f558189638af428e7714fc4d051c6c9dd3ee71e3
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-10 10:24:05 -0700

    Setup express server on port 3000

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,10 @@
+var express = require('express');
+var app = express();
 
+app.get('/', function(request, response) {
+  response.send('csv app');
+});
 
+app.listen(3000, function() {
+  console.log('app listening on port 3k');
+})
