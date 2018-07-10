dd9310c9a7fff62778c108db2935fd2972ab6806
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-10 10:33:12 -0700

    Move express setup and static file serving from server.js to app.js

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,10 +1,18 @@
+//set up express
 var express = require('express');
 var app = express();
 
+//serve the static HTML file
+app.use(express.static('client'));
+
+//get request
 app.get('/', function(request, response) {
   response.send('csv app');
 });
 
+
+//post request
+//start up the server
 app.listen(3000, function() {
   console.log('app listening on port 3k');
-})
+});
