0ec7548993b02f85baa33201d30e2be1a46e0994
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-11 11:21:07 -0700

    Setup basic express server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+//setup express server
+var express = require('express');
+var app = express();
 
+//set a port number
+app.listen(3000, function() {
+  console.log('app is running on 3k');
+});
 
+app.get('/', function(req, res) {
+  res.send('placeholder text');
+})
+//serve a static file 
