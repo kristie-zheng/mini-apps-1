f5d76562a02be3ae1b2671f1f3aab688a2b0efaf
Kristie Desiree <kly5@mail.sfsu.edu>
2018-07-11 11:28:08 -0700

    Allow express to serve static file

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -7,7 +7,8 @@ app.listen(3000, function() {
   console.log('app is running on 3k');
 });
 
-app.get('/', function(req, res) {
-  res.send('placeholder text');
-})
+// app.get('/', function(req, res) {
+//   res.send('placeholder text');
+// })
 //serve a static file 
+app.use(express.static('public'))
